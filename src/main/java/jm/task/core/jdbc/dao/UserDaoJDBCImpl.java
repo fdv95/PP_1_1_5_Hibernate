b0044439a5a08c2daa55bb6b273b4276e03ec7e8
Andrei Fadeev <fdv95@mail.ru>
2022-12-19 01:05:50 +0300

    Commit2

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,6 +1,5 @@
 package jm.task.core.jdbc.dao;
 
-
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
@@ -9,42 +8,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    final String INSERT_NEW = "INSERT INTO user (name, lastName, age) VALUES(?,?,?)"; // Внесение значений в таблицу
+    private final String CREAT_TABLE = "create table if not exists user (id bigint auto_increment,"
+            + " name varchar(45) not null, lastName varchar(45) not null, age tinyint not null,"
+            + " constraint user_pk primary key (id))"; // Создание таблицы
+    private final String INSERT_NEW = "INSERT INTO user (name, lastName, age) VALUES(?,?,?)"; // Внесение значений в таблицу
     private static final String GET_ALL = "SELECT * FROM user"; // Получение всех значений из таблицы
     private static final String DELETE = "DELETE FROM user WHERE id=?"; // Удаление значений по id
     private static final String DELETE_ALL = "DELETE FROM user"; // Удаление всех значений из таблицы
 
-
-    Connection connection = Util.getConnection();
     PreparedStatement preparedStatement = null;
 
     public UserDaoJDBCImpl() {
 
     }
 
+    @Override
     public void createUsersTable() {
-        try {
-            Statement statement = connection.createStatement();
-            statement.executeUpdate("create table if not exists user (id bigint auto_increment, name varchar(45) not null," +
-                    "lastName varchar(45) not null, age tinyint not null, constraint user_pk primary key (id))");
+        try (Connection connection = Util.getConnection()) {
+            preparedStatement = connection.prepareStatement(CREAT_TABLE);
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
+    @Override
     public void dropUsersTable() {
-        try {
-            Statement statement = connection.createStatement();
-            statement.executeUpdate("drop table if exists user");
+        try (Connection connection = Util.getConnection()) {
+            preparedStatement = connection.prepareStatement("drop table if exists user");
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
-        try {
+        try (Connection connection = Util.getConnection()) {
             preparedStatement = connection.prepareStatement(INSERT_NEW);
-//            preparedStatement.setLong(1,);
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
@@ -54,9 +55,10 @@ public class UserDaoJDBCImpl implements UserDao {
         }
     }
 
+    @Override
     public void removeUserById(long id) {
         User user = new User();
-        try {
+        try (Connection connection = Util.getConnection()) {
             preparedStatement = connection.prepareStatement(DELETE);
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
@@ -65,9 +67,10 @@ public class UserDaoJDBCImpl implements UserDao {
         }
     }
 
+    @Override
     public List<User> getAllUsers() {
         List<User> usersList = new ArrayList<>();
-        try {
+        try (Connection connection = Util.getConnection();) {
             preparedStatement = connection.prepareStatement(GET_ALL);
             ResultSet res = preparedStatement.executeQuery();
             while (res.next()) {
@@ -84,8 +87,9 @@ public class UserDaoJDBCImpl implements UserDao {
         }
     }
 
+    @Override
     public void cleanUsersTable() {
-        try {
+        try (Connection connection = Util.getConnection();) {
             preparedStatement = connection.prepareStatement(DELETE_ALL);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
